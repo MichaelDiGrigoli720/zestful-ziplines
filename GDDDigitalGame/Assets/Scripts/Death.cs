60a864c4d1e4bc79b78cf44228a3656db1f35592
Giovanni <Gio@Hastur.student.rit.edu>
2017-10-16 22:39:30 -0400

    Added basic hazard and player death scripts

---
+++ GDDDigitalGame/Assets/Scripts/Death.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+	public Vector3 startPoint = Vector3.zero;
+
+	private MonoBehaviour moveScript;
+	private Transform t;
+	private Vector3 initCamPos;
+
+	// Use this for initialization
+	void Start () {
+		moveScript = GetComponent("RigidbodyFirstPersonController") as MonoBehaviour;
+		t = GetComponent<Transform>();
+		initCamPos = t.GetChild(0).GetComponent<Transform>().position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public IEnumerator Respawn(float timeOut) {
+		yield return new WaitForSeconds(timeOut);
+		t.GetChild(0).GetComponent<Transform>().position = initCamPos;
+		t.position = startPoint;
+		moveScript.enabled = true;
+	}
+}
---
+++ GDDDigitalGame/Assets/Scripts/Hazard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		this.gameObject.tag = "Hazard";
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		GameObject gObj = collision.gameObject;
+		if(gObj.tag == "Player") {
+			Transform camTransform = gObj.GetComponent<Transform>().GetChild(0).GetComponent<Transform>();
+			camTransform.position -= camTransform.forward * 3;
+
+			(gObj.GetComponent("RigidbodyFirstPersonController") as MonoBehaviour).enabled = false;
+			IEnumerator coroute = gObj.GetComponent<Death>().Respawn(5.0f);
+			StartCoroutine(coroute);
+		}
+	}
+}
