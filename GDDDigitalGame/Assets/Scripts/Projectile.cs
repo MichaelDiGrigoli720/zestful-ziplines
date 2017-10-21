56475c2d25b08b877b76a0978d062e4df001fe4c
Giovanni <Gio@Hastur.student.rit.edu>
2017-10-20 23:50:07 -0400

    Started changing Zipline.cs to use projectiles. Still needs work

---
+++ GDDDigitalGame/Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public Zipline firedBy;
+	public float speed;
+
+	private Transform transform;
+
+	// Use this for initialization
+	void Start () {
+		transform = GetComponent<Transform>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * speed;
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		firedBy.loc = collision.contacts[0].point;
+		firedBy.isFlying = true;
+		firedBy.FPC.canMove = false;
+		firedBy.lineRenderer.enabled = enabled;
+		firedBy.lineRenderer.SetPosition(1, firedBy.loc);
+		Destroy(gameObject);
+	}
+}
--- GDDDigitalGame/Assets/Scripts/Zipline.cs
+++ GDDDigitalGame/Assets/Scripts/Zipline.cs
@@ -5,108 +5,78 @@ using UnityStandardAssets.Characters.FirstPerson;
 
 public class Zipline : MonoBehaviour
 {
-    public Camera cam;
-    public RaycastHit hit;
+	public Camera cam; //Player Camera
+	public GameObject projectile; //Projectile prefab
+	public int maxRange = 99999;
+	public bool isFlying; //Checks if they are in the air
+	public Vector3 loc; //Location of the hit point
+	public float speed = 10.0f; //Speed of the movement along the zipline
+	public float reloadTime = 1.0f; //Time before being able to shoot again
+	public Transform ziplineStart; //Zipline starting point (Hand for now)
+	public RigidbodyFirstPersonController FPC; // The FPS controller
+	public LineRenderer lineRenderer; //Zipline line
 
-    public LayerMask cullingMask;
-
-    public int maxRange = 99999;
-    public bool IsFlying;
-    public Vector3 loc;
-
-    public float reloadTime = 1.0f;
-    private float elapsedTime = 0.0f;
-
-    public float speed = 10;
-    
-    //Zipline starting point (Hand for now)
-    public Transform ziplineStart;
-
-    public RigidbodyFirstPersonController FPC;
-    private Rigidbody fpcRigidBody;
-
-    //Zipline line
-    public LineRenderer lineRenderer;
+	private float elapsedTime = 0.0f; //Time since last shot
+	private Rigidbody fpcRigidBody; // the FPC Rigidbody component
+	private GameObject curProj; //The current projectile shot
 
 	// Use this for initialization
 	void Start () {
-        Cursor.lockState = CursorLockMode.Locked;
-        fpcRigidBody = GetComponent<Rigidbody>();
+		Cursor.lockState = CursorLockMode.Locked;
+		fpcRigidBody = GetComponent<Rigidbody>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKey(KeyCode.Mouse0) && !isFlying && elapsedTime == 0.0f)
+		{
+			findLocation();
+			elapsedTime = reloadTime;
+		}
 
-        //Debug.DrawRay(cam.transform.position, Vector3.Lerp(transform.position, loc, (elapsedtime / maxHangTime)), Color.green);
-        if (Input.GetKey(KeyCode.Mouse0) && !IsFlying && elapsedTime == 0.0f)
-        {
-            print("Finding Loc");
-            findLocation();
-            elapsedTime = reloadTime;
-        }
+		if(isFlying)
+		{
+			fpcRigidBody.useGravity = false;
+			transform.position = Vector3.MoveTowards(transform.position, loc, speed * Time.deltaTime);
 
-        if(IsFlying)
-        {
-            fpcRigidBody.useGravity = false;
-            transform.position = Vector3.MoveTowards(transform.position, loc, speed * Time.deltaTime);
+			lineRenderer.SetPosition(0, ziplineStart.position);
+			//elapsedtime += Time.deltaTime;
 
-            lineRenderer.SetPosition(0, ziplineStart.position);
+			if (Vector3.Distance(transform.position, loc) < 5 && !Input.GetKey(KeyCode.Mouse0))
+			{
+				isFlying = false;
+				FPC.canMove = true;
+				lineRenderer.enabled = false;
+			}
+		}
 
-            //elapsedtime += Time.deltaTime;
+		else if(!isFlying && fpcRigidBody.useGravity == false)
+		{
+			fpcRigidBody.useGravity = true;
+		}
 
-            if (Vector3.Distance(transform.position, loc) < 5 && !Input.GetKey(KeyCode.Mouse0))
-            {
-                IsFlying = false;
-                FPC.canMove = true;
-                lineRenderer.enabled = false;
-            }
-        }
+		if (Input.GetKey(KeyCode.Space) && isFlying)
+		{
+			isFlying = false;
+			FPC.canMove = true;
+			lineRenderer.enabled = false;
+		}
 
-        else if(!IsFlying && fpcRigidBody.useGravity == false)
-        {
-            fpcRigidBody.useGravity = true;
-        }
+		if(elapsedTime != 0)
+		{
+			elapsedTime -= Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.Space) && IsFlying)
-        {
-            IsFlying = false;
-            FPC.canMove = true;
-            lineRenderer.enabled = false;
-        }
-
-        if(elapsedTime != 0)
-        {
-            elapsedTime -= Time.deltaTime;
-
-            if(elapsedTime <= 0)
-            {
-                elapsedTime = 0;
-            }
-        }
+			if(elapsedTime <= 0)
+			{
+				elapsedTime = 0;
+			}
+		}
 	}
 
-    public void findLocation()
-    {
-        print("Finding...");
-        print(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, maxRange, cullingMask));
-        //Raycasting to find where the zipline goes
-
-        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, maxRange, cullingMask))
-        {
-            //Get the location of the hit
-            loc = hit.point;
-
-            //Set the player to flying
-            IsFlying = true;
-
-            print(IsFlying + " " + loc);
-
-            //Make the player unable to move
-            FPC.canMove = false;
-
-            //Draw the line
-            lineRenderer.enabled = enabled;
-            lineRenderer.SetPosition(1, loc);
-        }
-    }
+	public void findLocation()
+	{
+		curProj = (GameObject)Instantiate(projectile, FPC.transform.position + FPC.transform.forward + new Vector3(0.0f, 0.75f, 0.0f), cam.transform.rotation);
+		curProj.GetComponent<Projectile>().firedBy = this;
+		Destroy(curProj, 5.0f);
+	}
 }
