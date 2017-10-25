a813276e7aeb4821ed0ad0e8d02aefa4f7b8dfe6
Ian Moon (RIT Student) <imm3350@ad.rit.edu>
2017-10-25 18:58:38 -0400

    Split Screeeen!
    Split Screen implemented. Crosshairs working.
    
    TO DO:
    Multiplayer Controls

--- GDDDigitalGame/Assets/Scripts/Crosshair.cs
+++ GDDDigitalGame/Assets/Scripts/Crosshair.cs
@@ -6,10 +6,11 @@ public class Crosshair : MonoBehaviour {
 
     public Texture2D crosshair;
     Rect position;
+    public Camera cam;
 
 	// Use this for initialization
 	void Start () {
-        position = new Rect((Screen.width - crosshair.width) / 2, (Screen.height - crosshair.height) / 2, crosshair.width, crosshair.height);
+        position = new Rect((cam.pixelWidth - crosshair.width)/2 + (cam.pixelWidth * (cam.rect.x * 2)), (cam.pixelHeight - crosshair.height) / 2, crosshair.width, crosshair.height);
     }
 	
 	// Update is called once per frame
